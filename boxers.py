88cfdfe0af44b70de490bb9206c98ecf29d05fc8
rosetastesoro <44842228+rosetastesoro@users.noreply.github.com>
2019-02-11 22:30:02 +0200

    Create boxers.py
    homework

---
+++ boxers.py
@@ -0,0 +1,11 @@
+class Boxers():
+
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+
+first_boxer = Boxers(age=33, weight=78, height=180)
+second_boxer = Boxers(age=28, weight=72, height=185)
+third_boxer = Boxers(age=35, weight=69, height=182)
